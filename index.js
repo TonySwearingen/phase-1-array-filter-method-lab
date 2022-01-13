36e655dff3b552c3c4a803d3766c55363adffea7
unlimited1974@aol.com <unlimited1974@aol.com>
2022-01-13 17:03:32 -0600

    commit tree 0bb4155859cb0762248efb871a00d4382523ef3d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching=(driverArray, enteredName)=>{
+    return (driverArray.filter(driverName=>driverName.toLowerCase()===enteredName.toLowerCase()))
+}
+
+const fuzzyMatch=(driverArray, enteredLetters)=>{
+    let endNum = enteredLetters.length
+
+    return (driverArray.filter(driverName=> {
+        return driverName.slice(0, endNum).toLowerCase() === enteredLetters.toLowerCase()
+    }))
+}
+
+const matchName = function (driverArray, string) {
+    let match = driverArray.filter((name) => name.name === string);
+    if (match) return match;
+}
+    
